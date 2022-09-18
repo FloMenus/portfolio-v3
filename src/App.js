86b61d34626515aa36e0cd0d3575852e9586dfcf
Florent Menus <florent.mns@gmail.com>
2022-09-18 18:55:50 +0200

    netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import Skill from "./components/Skill";
 import Project from "./components/Project";
--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 import MiniSkill from "./MiniSkill";
 
